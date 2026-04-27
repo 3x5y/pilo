ee7b6cc9f3185885a80ec827548fa3764b8cd1de
git <git@3x5y.art>
2026-04-27 16:15:26 +1000

    strengthen: ingest dir conflicts

---
+++ test/test_ingest_dir_conflict.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+INTAKE=/tank/data/active/pile-intake
+PILE=/tank/data/active/pile-readonly
+
+mkdir -p $INTAKE/foo
+echo good > $INTAKE/foo/file.txt
+
+system-ingest-pile
+
+# conflicting re-upload
+mkdir -p $INTAKE/foo
+echo bad > $INTAKE/foo/file.txt
+
+capture_status system-ingest-pile
+
+assert_command_fail
+echo "$OUTPUT" | assert_grep ERROR
+
+assert_grep good < $PILE/foo/file.txt
+assert_file_exists $INTAKE/foo/file.txt
