16007184efba32bdc6f8687601131e205c4fb03e
git <git@3x5y.art>
2026-04-27 10:56:04 +1000

    strengthen: promote verifies src and dst

---
+++ test/test_static_promote_checksum.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+FILE=file.txt
+SRC=/tank/data/active/pile-readonly/$FILE
+DST=/tank/data/static/collection/$FILE
+
+echo good > /tmp/$FILE
+system-capture /tmp/$FILE
+system-ingest-pile
+
+system-static-promote $FILE collection
+
+# verify integrity via checksum
+cmp "$SRC" "$DST" 2>/dev/null && fail "source should be removed"
+
+(cd /tank/data/static/collection && sha256sum --quiet --strict -c .manifest)
