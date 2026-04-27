92cc87c66c7259e427d4d87b9edac9fe7d5311c7
git <git@3x5y.art>
2026-04-27 11:02:51 +1000

    strengthen: promote detects conflict

---
+++ test/test_static_promote_conflict.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+FILE=file.txt
+
+echo good > /tmp/$FILE
+system-capture /tmp/$FILE
+system-ingest-pile
+system-static-promote $FILE collection
+
+# reintroduce conflicting version
+echo bad > /tmp/$FILE
+system-capture /tmp/$FILE
+system-ingest-pile
+
+capture_status system-static-promote $FILE collection
+
+assert_command_fail expected conflict
+echo "$OUTPUT" | assert_grep ERROR
+
+# original must remain
+assert_grep good < /tank/data/static/collection/$FILE
