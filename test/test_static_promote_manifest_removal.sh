6b26e8f8b0a485956bb67d9d44f62a558bb02281
git <git@3x5y.art>
2026-04-27 11:16:18 +1000

    strengthen: promotion removes from pile manifest

---
+++ test/test_static_promote_manifest_removal.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+FILE=file.txt
+PILE=/tank/data/active/pile-readonly
+
+echo data > /tmp/$FILE
+system-capture /tmp/$FILE
+system-ingest-pile
+
+system-static-promote $FILE collection
+
+cat $PILE/.manifest
+
+if grep -q "./$FILE$" $PILE/.manifest
+then
+    fail "file still present in pile manifest"
+fi
