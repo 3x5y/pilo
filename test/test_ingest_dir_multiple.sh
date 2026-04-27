aeb8f037288a7e773e6bbe9d8d879be6d176d58e
git <git@3x5y.art>
2026-04-27 15:04:03 +1000

    strengthen: ingest multiple dirs

---
+++ test/test_ingest_dir_multiple.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+INTAKE=/tank/data/active/pile-intake
+PILE=/tank/data/active/pile-readonly
+
+mkdir -p $INTAKE/a
+mkdir -p $INTAKE/b
+
+echo one > $INTAKE/a/1.txt
+echo two > $INTAKE/b/2.txt
+
+system-ingest-dir
+
+assert_file_exists $PILE/in/a/1.txt
+assert_file_exists $PILE/in/b/2.txt
