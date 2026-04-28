ac6e3a98d3f0c882cd572fe3ff5afd69558a528c
git <git@3x5y.art>
2026-04-28 17:05:07 +1000

    feat: pile rewrite allow batch ops

---
+++ test/test_pile_move_batch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+mkfile a a.txt
+mkfile b b.txt
+
+capture_file a.txt
+capture_file b.txt
+system-ingest-pile
+
+system-rewrite "
+mv in/a.txt sort/a.txt
+mv in/b.txt sort/b.txt
+"
+
+assert_file_exists /$PILE/sort/a.txt
+assert_file_exists /$PILE/sort/b.txt
+
+assert_manifest_valid /$PILE
