50e93b3dabf6d37923f4a4fd778e0b0096ab0c11
git <git@3x5y.art>
2026-04-28 16:15:53 +1000

    strengthen: pile move is idempotent

---
+++ test/test_pile_move_idempotent.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+file=file.txt
+mkfile data $file
+capture_file $file
+system-ingest-pile
+
+system-rewrite "mv in/$file sort/$file"
+system-rewrite "mv in/$file sort/$file" >/dev/null || true
+
+assert_file_exists /$PILE/sort/$file
+assert_manifest_valid /$PILE
