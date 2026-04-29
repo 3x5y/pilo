ddc44319f5b604934566562a9c34223a5d2cafbb
git <git@3x5y.art>
2026-04-29 12:32:37 +1000

    strengthen: repl safe add post verify step

---
+++ test/test_repl_safe_postverify.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+system-snapshot t0
+system-replicate
+
+system-snapshot t1
+
+system-replicate-safe
+
+capture_status system-replication-verify
+assert_command_ok expected system to be consistent after safe replication
