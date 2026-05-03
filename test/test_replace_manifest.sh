95c195f553c15eff349f417eac4c9271f12e1aa1
git <git@3x5y.art>
2026-05-03 15:49:33 +1000

    strengthen: replace manifest

---
+++ test/test_replace_manifest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+mkfile old file.txt
+capture_file file.txt
+pilo ingest-pile
+pilo manifest-update
+
+echo new > "$TMP/new.txt"
+
+pilo replace "$TMP/new.txt" in/file.txt
+
+assert_manifest_valid pile "$PILO_PILE_PATH"
