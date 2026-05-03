9f9b7f1e38db83004261561c8035c7b80e3c19c4
git <git@3x5y.art>
2026-05-03 17:24:56 +1000

    refactor: replace use with_writable and tidy up

--- src/pilo-replace.sh
+++ src/pilo-replace.sh
@@ -4,43 +4,37 @@ set -eu
 SRC="$1"
 DST_REL="$2"
 
-file_exists "$SRC" || fatal "source file missing: $SRC"
-
-dataset_for_path() {
-    case "$1" in
-        in/*)
-            echo "$PILO_PILE_DATASET"
-            ;;
-        collection/*)
-            echo "$PILO_STATIC_DATASET/collection"
-            ;;
-        filing/*)
-            sub=$(echo "$1" | cut -d/ -f2)
-            echo "$PILO_STATIC_DATASET/filing/$sub"
-            ;;
-    esac
+replace_file() {
+    local src=$1
+    local dst=$2
+    cp "$src" "$dst"
+    chown "$PILO_USER":"$PILO_USER" "$dst"
 }
 
 
 case "$DST_REL" in
-    in/*)
+    in/*|out/*|sort/*)
         DST="$PILO_PILE_PATH/$DST_REL"
         DATASET="$PILO_PILE_DATASET"
         ;;
-    collection/*|filing/*)
+    collection/*)
         DST="$PILO_STATIC_PATH/$DST_REL"
-        DATASET=$(dataset_for_path "$DST_REL")
+        DATASET="$PILO_STATIC_DATASET/collection"
+        ;;
+    filing/*)
+        DST="$PILO_STATIC_PATH/$DST_REL"
+        sub=$(echo "$DST_REL" | cut -d/ -f2)
+        DATASET="$PILO_STATIC_DATASET/filing/$sub"
         ;;
     *)
         fatal "invalid target path"
         ;;
 esac
 
+file_exists "$SRC" || fatal "source file missing: $SRC"
 file_exists "$DST" || fatal "target does not exist: $DST_REL"
 
-zfs set readonly=off "$DATASET"
-cp "$SRC" "$DST"
-chown "$PILO_USER":"$PILO_USER" "$DST"
-zfs set readonly=on "$DATASET"
+with_writable "$DATASET" \
+    replace_file "$SRC" "$DST"
 
 pilo manifest-update
---
+++ test/test_replace_filing.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+archive=filing/2025
+mkfile old file.txt
+capture_file file.txt
+pilo ingest-pile
+
+with_writable $PILE \
+    mkdir -p "$PILO_PILE_PATH/out/$archive"
+with_writable $PILE \
+    mv "$PILO_PILE_PATH/in/file.txt" \
+       "$PILO_PILE_PATH/out/$archive/file.txt"
+
+zfs create -p $STATIC/$archive
+pilo static-promote
+
+echo new > "$TMP/new.txt"
+
+pilo replace "$TMP/new.txt" $archive/file.txt
+
+grep -q new "$PILO_STATIC_PATH/$archive/file.txt" \
+    || fail "static replace failed"
