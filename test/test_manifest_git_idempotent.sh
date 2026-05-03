1d6f4448f1e17d51ae80ef351b4880d8739c836f
git <git@3x5y.art>
2026-05-03 19:04:20 +1000

    strengthen: manifest git idempotent

---
+++ test/test_manifest_git_idempotent.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+mkfile data a.txt
+capture_file a.txt
+pilo ingest-pile
+
+with_writable $COLLECTION \
+    touch "$PILO_STATIC_PATH"/collection/b.txt
+
+pilo manifest-update
+before=$(runuser git -C "$PILO_ADMIN_PATH/manifest" rev-parse HEAD)
+
+pilo manifest-update
+after=$(runuser git -C "$PILO_ADMIN_PATH/manifest" rev-parse HEAD)
+
+[ "$before" = "$after" ] || fail "manifest update not idempotent"
