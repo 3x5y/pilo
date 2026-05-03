fb4a16ee012e931d6ee1d540abaa96c1d692b9f8
git <git@3x5y.art>
2026-05-03 13:19:57 +1000

    feat: manifest uses git

--- src/pilo-manifest-update.sh
+++ src/pilo-manifest-update.sh
@@ -11,4 +11,9 @@ cd $pile
 generate_manifest > "$tmp"
 
 as_user mkdir -p "$PILO_ADMIN_PATH"/manifest
+if [ ! -d "$PILO_ADMIN_PATH/.git" ]
+then
+    git -c init.defaultBranch=master \
+        init "$PILO_ADMIN_PATH" >/dev/null
+fi
 as_user cp $tmp "$PILO_ADMIN_PATH"/manifest/pile.manifest
---
+++ test/test_manifest_git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+mkfile data file.txt
+capture_file file.txt
+pilo ingest-pile
+
+pilo manifest-update
+
+assert_dir_exists "$PILO_ADMIN_PATH/.git"
