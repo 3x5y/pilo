97d380863f117212b126e709cf9fd50a48cfddcc
git <git@3x5y.art>
2026-05-03 17:54:28 +1000

    extend: static manifest in git also

--- src/pilo-manifest-update.sh
+++ src/pilo-manifest-update.sh
@@ -2,15 +2,10 @@
 set -eu
 
 pile=$PILO_PILE_PATH
+static=$PILO_STATIC_PATH
 manifest_dir="$PILO_ADMIN_PATH"/manifest
-manifest="$manifest_dir"/pile.manifest
-
-tmp=$(tmpfile)
-chmod +r $tmp
-
-cd $pile
-generate_manifest > "$tmp"
-add_tmpfile_cleanup $tmp
+pile_manifest="$manifest_dir"/pile.manifest
+coll_manifest="$manifest_dir"/collection.manifest
 
 if [ ! -d "$manifest_dir/.git" ]
 then
@@ -19,8 +14,27 @@ then
             init "$manifest_dir" >/dev/null
 fi
 
-as_user cp $tmp "$manifest"
-as_user git -C "$manifest_dir" add "$manifest"
+tmp_pile=$(tmpfile)
+tmp_coll=$(tmpfile)
+chmod +r $tmp_pile $tmp_coll
+add_tmpfile_cleanup $tmp_pile $tmp_coll
+
+(
+    cd $pile
+    generate_manifest
+) > "$tmp_pile"
+
+(
+    cd $static/collection
+    generate_manifest
+) > $tmp_coll
+
+as_user cp $tmp_pile "$pile_manifest"
+as_user git -C "$manifest_dir" add "$pile_manifest"
+
+as_user cp $tmp_coll "$coll_manifest"
+as_user git -C "$manifest_dir" add "$coll_manifest"
+
 if ! as_user git -C "$manifest_dir" diff --quiet --cached
 then
     as_user git -C "$manifest_dir" commit -m "manifest update" >/dev/null
