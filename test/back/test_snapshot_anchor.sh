09be9ddc8b61a6c06fb1e3b0d86db244868c26e6
git <git@3x5y.art>
2026-05-05 12:15:00 +1000

    test consolidation part 1: snapshots

--- test/back/test_snapshot_anchor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-pilo snapshot-anchor
-
-zfs list -t snapshot | assert_grep "@a-"
--- test/back/test_snapshot_rpo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-pilo snapshot-rpo
-
-zfs list -t snapshot | assert_grep "@r-"
