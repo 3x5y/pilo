47e92665263766e76da1bc448d2286adff670bc7
git <git@3x5y.art>
2026-05-05 12:33:58 +1000

    test consolidation part 5: anchors

--- test/back/test_repl_anchor_daily.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-capture_status pilo anchor-create daily
-
-assert_command_ok failed to create daily anchor
-
-zfs list -t snap | assert_grep "@daily-"
--- test/back/test_repl_anchor_preferred.sh
+++ test/back/test_repl_anchor_preferred.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
+pilo snapshot t0
+pilo replicate
+
 pilo anchor-create rotation
 pilo replicate
 
 pilo snapshot t1
-
-capture_status pilo replicate
-
-assert_command_ok replication failed
+pilo replicate
 
 # ensure incremental basis was anchor (indirectly)
 zfs list -t snap $TEST_REPLICA | assert_grep rotation-
