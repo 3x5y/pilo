3b54b7e008c2652561b8433e0f0c32222b5d4181
git <git@3x5y.art>
2026-05-05 13:11:16 +1000

    test consolidation part 11: init

--- test/back/test_init_paths.sh
+++ test/back/test_init_paths.sh
@@ -6,7 +6,9 @@ unset PILO_INTAKE_PATH
 unset PILO_PILE_PATH
 unset PILO_STATIC_PATH
 
-pilo init
+capture_status pilo init
+
+assert_command_ok "init should succeed with valid layout"
 
 [ -d "$PILO_PATH/active/admin" ] || fail "default admin path missing"
 [ -d "$PILO_PATH/active/pile-intake" ] || fail "default intake path missing"
