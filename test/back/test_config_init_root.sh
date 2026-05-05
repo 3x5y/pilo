63ab13c35d84a09fe6a5b13fbb8621d58c5c3b07
git <git@3x5y.art>
2026-05-05 13:03:12 +1000

    test consolidation part 8: config

--- test/back/test_config_init_root.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-reset_system tank/test/test-alt
-
-assert_dir_exists $PILE_PATH/in
-assert_dir_exists $PILE_PATH/out/collection
-[ $(zfs get -H -o value readonly $PILE) = on ] \
-    || fail $PILE not readonly after init
